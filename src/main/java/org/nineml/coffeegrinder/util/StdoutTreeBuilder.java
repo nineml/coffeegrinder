5795a71e88c0e5aca1af3320296f2c108db4943b
Norman Walsh <ndw@nwalsh.com>
2022-09-10 17:13:49 +0100

    Refactored the tree builders

--- src/main/java/org/nineml/coffeegrinder/util/StdoutTreeBuilder.java
+++ src/main/java/org/nineml/coffeegrinder/util/StdoutTreeBuilder.java
@@ -11,47 +11,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-public class StdoutTreeBuilder extends PriorityTreeBuilder {
-    private static final String tab = "  ";
-    private String indent = "";
-    private StringBuilder sb = null;
-
-    @Override
-    public void startNonterminal(NonterminalSymbol symbol, Map<String,String> attributes, int leftExtent, int rightExtent) {
-        if (sb != null) {
-            System.out.printf("%s%s%n", indent, sb);
-            sb = null;
-        }
-        System.out.printf("%s<%s>%n", indent, symbol);
-        indent = indent + tab;
-    }
-
-    @Override
-    public void endNonterminal(NonterminalSymbol symbol, Map<String,String> attributes, int leftExtent, int rightExtent) {
-        if (sb != null) {
-            System.out.printf("%s%s%n", indent, sb);
-            sb = null;
-        }
-        indent = indent.substring(tab.length());
-        System.out.printf("%s</%s>%n", indent, symbol);
-    }
-
-    @Override
-    public void token(Token token, Map<String,String> attributes) {
-        if (sb == null) {
-            sb = new StringBuilder();
-        }
-        if (token instanceof TokenCharacter) {
-            int cp = ((TokenCharacter) token).getCodepoint();
-            if (cp == '<' || cp == '>' || cp == '&' || cp <= ' ') {
-                sb.append(String.format("&#x%x;", cp));
-            } else {
-                sb.appendCodePoint(cp);
-            }
-        } else if (token instanceof TokenString) {
-            sb.append(token.getValue());
-        } else {
-            sb.append(token.toString());
-        }
+public class StdoutTreeBuilder extends PrintStreamTreeBuilder {
+    public StdoutTreeBuilder() {
+        super(System.out);
     }
 }
